e9418b9b027175cc6550e0734c175353b21bc0f1
Pavel Bakatov <pbakatov@gmail.com>
2017-12-17 21:18:25 +0200

    spec

--- node_subnet/spec/unit/ohai_plugins/node_subnet_module_spec.rb
+++ node_subnet/spec/unit/ohai_plugins/node_subnet_module_spec.rb
@@ -1,1 +1,16 @@
 require 'spec_helper'
+
+describe_ohai_plugin :Apache do
+  let(:plugin_file) { 'files/default/node_subnet_module.rb' }
+
+  it 'provides apache/modules' do
+    expect(plugin).to provides_attribute('"hostname", "ENV"')
+  end
+
+  let(:command) { double('Fake Command',stdout: 'OUTPUT') }
+
+  it 'correctly captures output' do
+    allow(plugin).to receive(:shell_out).with('ip -o -f inet addr show | awk '/scope global/ {print $4}'').and_return(command)
+    expect(plugin_attribute('"hostname", "ENV"')).to eq('OUTPUT')
+  end
+end
